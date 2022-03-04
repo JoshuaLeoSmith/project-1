742e37f1fb002ebc2843e32ef642d563a405895f
Caleb Kirschbaum <calebhk98@gmail.com>
2022-03-04 17:04:52 -0600

    Added tableDao

--- project-1-ORM/src/main/java/com/revature/dao/TableDao.java
+++ project-1-ORM/src/main/java/com/revature/dao/TableDao.java
@@ -1,16 +1,119 @@
 package com.revature.dao;
 
-//import org.apache.log4j.Logger;
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+//import ;
+//import java.;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.revature.annotations.Id;
+import com.revature.util.ConnectionUtil;
 
 
 public class TableDao {
-	//private static Logger logBot=Logger.getLogger(TableDao.class);
-	
+	private static Logger logBot=Logger.getLogger(TableDao.class);
+	private static String managment= ConnectionUtil.getManagement();
+	private static String schema= ConnectionUtil.getSchema();
+	private static int String =0;
+	private static Connection conn = ConnectionUtil.getConnection();
 
-	public void insert(){
+	public void insert(String name, List<Field> allFieldsInTable) throws IllegalAccessException{
+		
+		if(managment.toLowerCase().equals("validate")) {
+			throw new IllegalAccessException("Can't insert tables on validate control. Change control to insert.");
+		}	
+		String sql ="create table";
+		/*	validate: validate the schema, makes no changes to the database.
+			update: update the schema.
+			create: creates the schema, destroying previous data.
+			
+		 * */
+		if(managment.toLowerCase().equals("update")) {
+			sql += " if not exists";
+		}
+		sql += " "+schema+"."+name+"(";
+		
+		String fieldName;
+		String modifications="";
+		for (Field myField : allFieldsInTable) {
+			fieldName=myField.getName();//Correct the naming convention
+			if(myField.getAnnotation(Id.class) != null) {
+				modifications+=" serial primary key";
+			}
+			else if(myField.getType()==int.class || myField.getType()==byte.class||myField.getType()==short.class||myField.getType()==long.class) {
+				modifications+=" int";
+			}//Test against Integer
+			else if(myField.getType()==float.class || myField.getType()==double.class) {
+				modifications+=" numeric";
+			}
+			else if(myField.getType()==boolean.class) {
+				modifications+=" boolean";
+			}
+			else if(myField.getType()==char.class) {
+				modifications+=" char(1)";
+			}
+			else if(myField.getType()==String.class ) {
+				modifications+=" text";
+			}
+			else if(myField.getType()==java.util.Date.class || myField.getType()==java.sql.Date.class ||myField.getType()==java.time.LocalDate.class) {
+				modifications+=" date";
+			}
+			/*if(myField. ) {
+				modifications+=" not null";
+			}
+			if(myField. ) {
+				modifications+=" unique";
+			}
+			if(myField. ) {
+				modifications+=" default "+defaultValue;
+			}
+			if(myField. ) {
+				modifications+=" references "+refrenceValue +" on delete cascade";
+			}
+			*
+			*
+			*
+			*/ //
+			modifications+=",";
+			
+			sql+=modifications;
+		}
+		sql= sql.substring(0, sql.length() - 1);
+		sql +=");";
+		
+		try {
+			PreparedStatement myStatment= conn.prepareStatement(sql);
+			
+			
+			System.out.println(myStatment.toString());//Uncomment if throwing errors to see what is being queried
+			
+			myStatment.execute();
+			
+		} catch (SQLException e) {
+			logBot.error("Database Error, unable to run");
+			e.printStackTrace();
+		}
+		
+		
+		
 		
 	}
 
+	
+	/*public static void main(String[] args) {
+		int rounds=0;
+		int name=0;
+		List<Field> test=new ArrayList<>();
+		test.add( rounds);
+		
+		insert("Weapons", test);
+	}*/
+	
 
 }
 
