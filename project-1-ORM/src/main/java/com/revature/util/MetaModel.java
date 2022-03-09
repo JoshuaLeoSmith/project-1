1f93a17ca2a8d38d16f3f2fd580ea0bca319c496
nrblichf <nrblichf@mtu.edu>
2022-03-09 16:54:04 -0600

    added getTableName to MetaModel

--- project-1-ORM/src/main/java/com/revature/util/MetaModel.java
+++ project-1-ORM/src/main/java/com/revature/util/MetaModel.java
@@ -2,8 +2,10 @@ package com.revature.util;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import java.util.function.BinaryOperator;
 
 import com.revature.annotations.Column;
@@ -18,7 +20,7 @@ import com.revature.annotations.JoinColumn;
  * 
  * This class' job is to model data about another class.
  */
-public class MetaModel<T> { 
+public class MetaModel<T> {
 
 	private Class<?> clazz;
 	private PrimaryKeyField primaryKeyField;
@@ -49,27 +51,27 @@ public class MetaModel<T> {
 	public List<ColumnField> getColumns() {
 		if (!columnFields.isEmpty())
 			return columnFields;
-		
+
 		Field[] fields = clazz.getDeclaredFields();
-		
 
 		// check if each field has a @Column annotation
 		// if it does, add it to the metamodel's columnField's list
 		for (Field field : fields) {
-			
-			// the column reference variable will NOT be null if the field is indeed
-			// annotated with @Column
-			Column column = field.getAnnotation(Column.class);
-			Exclude exclude = field.getAnnotation(Exclude.class);
-			Id id = field.getAnnotation(Id.class);
-			JoinColumn jColumn = field.getAnnotation(JoinColumn.class);
+			if (!field.isSynthetic()) {
+				// the column reference variable will NOT be null if the field is indeed
+				// annotated with @Column
+				Column column = field.getAnnotation(Column.class);
+				Exclude exclude = field.getAnnotation(Exclude.class);
+				Id id = field.getAnnotation(Id.class);
+				JoinColumn jColumn = field.getAnnotation(JoinColumn.class);
 
-			if (column != null || (exclude == null && id == null && jColumn == null)) {
-				// if the column is indeed marked with @Colum, instantiate a new ColumnField
-				// object with its data
-				columnFields.add(new ColumnField(field));
-				// now that we've transposed the field to a column Field object, we can capture
-				// data like Type, columnName, etc...
+				if (column != null || (exclude == null && id == null && jColumn == null)) {
+					// if the column is indeed marked with @Colum, instantiate a new ColumnField
+					// object with its data
+					columnFields.add(new ColumnField(field));
+					// now that we've transposed the field to a column Field object, we can capture
+					// data like Type, columnName, etc...
+				}
 			}
 		}
 		if (columnFields.isEmpty()) {
@@ -83,14 +85,14 @@ public class MetaModel<T> {
 	public PrimaryKeyField getPrimaryKey() {
 		if (primaryKeyField != null)
 			return primaryKeyField;
-		
+
 		Field[] fields = clazz.getDeclaredFields();
 
 		for (Field field : fields) {
 
 			Id primaryKey = field.getAnnotation(Id.class);
 
-			if (primaryKey != null) { // we se the primaryKeyField of this MetaModel to the field we find 
+			if (primaryKey != null) { // we se the primaryKeyField of this MetaModel to the field we find
 				this.primaryKeyField = new PrimaryKeyField(field);
 				return new PrimaryKeyField(field); // if there is a PK found, we end the method here and return it
 			}
@@ -103,7 +105,7 @@ public class MetaModel<T> {
 	public List<ForeignKeyField> getForeignKeys() {
 		if (!foreignKeyFields.isEmpty())
 			return foreignKeyFields;
-		
+
 		Field[] fields = clazz.getDeclaredFields();
 
 		for (Field field : fields) {
@@ -119,62 +121,68 @@ public class MetaModel<T> {
 		}
 		return foreignKeyFields;
 	}
-	
+
 	public String getSimpleClassName() {
 		return clazz.getSimpleName();
 	}
-	
+
 	public String getClassName() {
-		return clazz.getName(); // reutrns  the package of where the class came from as well
+		return clazz.getName(); // reutrns the package of where the class came from as well
 	}
 	
-	// to use this function, you must first call all of the get methods of MetaModel for each instance before
-	// calling this function so that all of the field have had at least one chance to be initialized
-	public GenericField getFieldByName(String name){
+	public String getTableName() {
+		return clazz.getAnnotation(Entity.class).tableName();
+	}
+
+	// to use this function, you must first call all of the get methods of MetaModel
+	// for each instance before
+	// calling this function so that all of the field have had at least one chance
+	// to be initialized
+	public GenericField getFieldByName(String name) {
 		if (primaryKeyField != null) {
 			if (primaryKeyField.getName().equals(name) || primaryKeyField.getColumnName().equals(name)) {
 				return primaryKeyField;
 			}
 		}
-		
-		for (ForeignKeyField fkField: foreignKeyFields) {
+
+		for (ForeignKeyField fkField : foreignKeyFields) {
 			if (fkField.getName().equals(name) || fkField.getColumnName().equals(name)) {
 				return fkField;
 			}
 		}
-		
+
 		for (ColumnField cField : columnFields) {
 			if (cField.getName().equals(name) || cField.getColumnName().equals(name)) {
 				return cField;
 			}
 		}
-		
+
 		throw new RuntimeException("Unable to find field \"" + name + "\" in class \"" + this.getClassName() + "\"");
 	}
 
-	// to use this function, you must first call all of the get methods of MetaModel for each instance before
-	// calling this function so that all of the field have had at least one chance to be initialized
-	public List<GenericField> getAllFields() {
-		List<GenericField> allFields = new ArrayList<>();
+	// to use this function, you must first call all of the get methods of MetaModel
+	// for each instance before
+	// calling this function so that all of the field have had at least one chance
+	// to be initialized
+	public Set<GenericField> getAllFields() {
+		Set<GenericField> allFields = new HashSet<>();
 		if (primaryKeyField != null)
 			allFields.add(primaryKeyField);
 		
 		allFields.addAll(foreignKeyFields);
-		
 		allFields.addAll(columnFields);
-		
+
 		return allFields;
 	}
-  
+
 	public List<Object> getAllValsOfField(Object o) {
-		
-		
+
 		Field[] fields = clazz.getDeclaredFields();
-		
+
 		List<Object> fieldValues = new LinkedList<Object>();
-		
-		for(Field f : fields) {
-			
+
+		for (Field f : fields) {
+
 			try {
 				f.setAccessible(true);
 				fieldValues.add(f.get(o));
