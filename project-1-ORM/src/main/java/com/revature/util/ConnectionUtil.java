bd315c34ada3674c5b4790e68be24c8efb790282
Caleb Kirschbaum <calebhk98@gmail.com>
2022-03-14 16:29:31 -0500

    Added connection pooling

--- project-1-ORM/src/main/java/com/revature/util/ConnectionUtil.java
+++ project-1-ORM/src/main/java/com/revature/util/ConnectionUtil.java
@@ -1,25 +1,29 @@
 package com.revature.util;
 
+import java.beans.PropertyVetoException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+
 import org.apache.log4j.Logger;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionUtil {
-	private static Logger logBot=Logger.getLogger(ConnectionUtil.class);	
+	private static Logger logBot=Logger.getLogger(ConnectionUtil.class);
+	private static ComboPooledDataSource cpds = new ComboPooledDataSource();
+	private static boolean connected = false;
 	private static Connection conn=null;
 	private ConnectionUtil() {}
-	
-	public static String getSchema() {	
+
+	public static String getSchema() {
 		try {
-			Properties prop=new Properties();		
-			prop.load(new FileReader( "src/main/resources/config.properties"));			
-			String schema=prop.getProperty("schema");
-			return schema;
+			Properties prop=new Properties();
+			prop.load(new FileReader( "src/main/resources/config.properties"));
+			return prop.getProperty("schema");
 		} catch (FileNotFoundException e) {
 			logBot.error("No file exists for the properties");
 			e.printStackTrace();
@@ -29,14 +33,13 @@ public class ConnectionUtil {
 		}
 		return "public";
 	}
-	
-	
-	public static String getManagement() {	
+
+
+	public static String getManagement() {
 		try {
-			Properties prop=new Properties();		
-			prop.load(new FileReader( "src/main/resources/config.properties"));			
-			String schema=prop.getProperty("SchemaManagement");
-			return schema;
+			Properties prop=new Properties();
+			prop.load(new FileReader( "src/main/resources/config.properties"));
+			return prop.getProperty("SchemaManagement");
 		} catch (FileNotFoundException e) {
 			logBot.error("No file exists for the properties");
 			e.printStackTrace();
@@ -46,34 +49,52 @@ public class ConnectionUtil {
 		}
 		return "create";
 	}
-	
-	
-	public static Connection getConnection() {		
+
+	private static void startConnection() {
 		try {
-			if(conn!=null && !conn.isClosed()) {
-				return conn;
-			}
-		} catch (SQLException e) {
-			logBot.error("The connection has an error");
-			e.printStackTrace();
-		}		
-		try {
-			Properties prop=new Properties();		
-			prop.load(new FileReader( "src/main/resources/config.properties"));			
+			Properties prop=new Properties();
+			prop.load(new FileReader( "src/main/resources/config.properties"));
 			String url=prop.getProperty("url");
 			String userName=prop.getProperty("username");
-			String password=prop.getProperty("password");	
-			conn = DriverManager.getConnection(url, userName, password);
+			String password=prop.getProperty("password");
+
+			cpds.setDriverClass("org.postgresql.Driver"); // loads the jdbc driver
+			cpds.setJdbcUrl(url);
+			cpds.setUser(userName);
+			cpds.setPassword(password);
+			cpds.setMaxPoolSize(10);
+			cpds.setMinPoolSize(1);
 		} catch (FileNotFoundException e) {
 			logBot.error("No file exists for the properties");
 			e.printStackTrace();
 		} catch (IOException e) {
 			logBot.error("Can't read from the properties file");
 			e.printStackTrace();
+		} catch (PropertyVetoException e) {
+			logBot.error("PropertyVetoException e");
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() {
+		try {
+			if ((conn != null) && !conn.isClosed()) {
+				return conn;
+			}
+		} catch (SQLException e) {
+			logBot.error("The connection has an error");
+			e.printStackTrace();
+		}
+
+		try {
+			if (!connected) {
+				startConnection();
+			}
+			conn = cpds.getConnection();
 		} catch (SQLException e) {
 			logBot.error("Unable to connect to the database");
 			e.printStackTrace();
-		}		
+		}
 		return conn;
-	}	
+	}
 }
