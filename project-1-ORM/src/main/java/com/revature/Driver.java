0b23db4d19a1d1ad4e5b7902771fe271225f99e3
JoshuaLeoSmith <joshualeosmith1@gmail.com>
2022-03-14 15:54:45 -0400

    update almost finished

--- project-1-ORM/src/main/java/com/revature/Driver.java
+++ project-1-ORM/src/main/java/com/revature/Driver.java
@@ -11,10 +11,13 @@ public class Driver {
 
 		//i.insert(new TesterClass("uname", "pwd", 12.22), true);
 		
-		TesterClass t = (TesterClass) i.findByPk(TesterClass.class, 1);
+		i.create(TesterClass.class);
 		
-		
+		i.insert(new TesterClass(1,"uname1", "pwd", 12.22), true);
+		i.insert(new TesterClass(1, "uname3", "pwd", 12.22), true);
+
+		//i.insert(new TesterClass("uname1", "pwd1", 12.55), true);
+	
 		
 	}
-
 }
